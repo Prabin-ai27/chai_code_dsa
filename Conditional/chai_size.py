775d0226c1b5ed441d49f329a85e3b50bbd82c93
Prabin-ai27 <prabinkumarpradhan1205@gmail.com>
2026-09-08 14:17:55 +0530

    two file upadte

---
+++ Conditional/chai_size.py
@@ -0,0 +1,10 @@
+is_chia_size=input("Enter your chai size small,medium.large: ").lower()
+
+if is_chia_size=="small":
+    print("Chai prise is 5 rupees")
+elif is_chia_size=="medium":
+    print("Chai prise is 10 rupees")
+elif is_chia_size=="large":
+    print("Chai prise is 15 rupees")
+else:
+    print("Selected: Unoknow size")
---
+++ Conditional/snack_story.py
@@ -0,0 +1,6 @@
+is_snack_input=input("Enter your preffered snack:")
+
+if is_snack_input == "somasa" or is_snack_input == "cookies":
+    print("Well Sir ! your order is take.......")
+else:
+    print("Sorry,You take only somasa and cookies")
