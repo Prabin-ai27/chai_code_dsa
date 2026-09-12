ddca5a15adbaf6c12c5175f774394c82a95dffc8
snigdhas-code <snigdharanibeherasb@gmail.com>
2026-09-13 05:14:04 +0530

    array largest numberuse for loop

--- js_interview/array/largestNumber.js
+++ js_interview/array/largestNumber.js
@@ -6,3 +6,16 @@ function largestNum(input){
     console.log(Math.max(...input))
 }
 largestNum(Input)
+
+
+function larNum(input){
+    let largest=input[0]
+    for(let i=1;i<input.length;i++){
+        if(input[i]>largest){
+            largest=input[i]
+        }
+    }
+    return largest
+}
+
+console.log(larNum(Input))
