f3c240cef91133a251ca05e94dcba6dab0dc3dbc
snigdhas-code <snigdharanibeherasb@gmail.com>
2026-09-16 10:02:03 +0530

    How to use walrus

---
+++ 02_loops/06_walrus.py
@@ -0,0 +1,9 @@
+# normal code
+
+name="prabin"
+print(F"Normal code way:- {name}")
+
+# Walrus way how to do:-":="
+print(name:="Prasni")
+
+print(f"How to show after you use walrus{(name :='Prabin')}")
