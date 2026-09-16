1049cc8b6e5e30553a75de3434b4ab6b3dd8282d
snigdhas-code <snigdharanibeherasb@gmail.com>
2026-09-16 10:16:54 +0530

    more code og walrus

--- 02_loops/06_walrus.py
+++ 02_loops/06_walrus.py
@@ -7,3 +7,9 @@ print(F"Normal code way:- {name}")
 print(name:="Prasni")
 
 print(f"How to show after you use walrus{(name :='Prabin')}")
+
+
+if (age:=int(input('Enter your Age:-')))>=18:
+    print('You are allowde on this web side')
+else:
+    print(f"Tu mera Bacha he come after {18-age}")
